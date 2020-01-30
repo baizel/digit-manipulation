d9f6fa7000c3c49d15fb62a7d5d2ed9aaeaa6bd7
baizel <baizelmathew@yahoo.co.in>
2020-01-30 22:07:58 +0000

    Add initial solution

--- src/main/java/com/and/test/Solution.java
+++ src/main/java/com/and/test/Solution.java
@@ -1,8 +1,6 @@
 package com.and.test;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
@@ -11,11 +9,60 @@ public class Solution {
      */
 
     public static String solution(String input) throws NumberFormatException {
-        return null;
+        Map<Integer, Integer> intLookUp = getIntAndCount(input);
+        List<Integer> digit = new ArrayList<>(intLookUp.keySet());
+
+        if (digit.size() == 0) {
+            throw new NumberFormatException("No integers in given input");
+        }
+        digit.sort(Collections.reverseOrder());
+
+        StringBuilder allSiblings = new StringBuilder();
+
+        permutation(digit, intLookUp,new char[digit.size()],0,allSiblings);
+        allSiblings.deleteCharAt(allSiblings.length()-1); //delete the trailing comma
+
+        return allSiblings.toString();
     }
 
+    public static void permutation(List<Integer> digits, Map<Integer, Integer> occurrencesLookUp, char[] combinationResult, int recursionLevel, StringBuilder allSiblings) {
+        if (recursionLevel == combinationResult.length) {
+            allSiblings.append(new String(combinationResult));
+            allSiblings.append(",");
+            return;
+        }
+
+        for (int i = 0; i < digits.size(); i++) {
+            if (occurrencesLookUp.get(digits.get(i)) == 0) {
+                continue;
+            }
+            combinationResult[recursionLevel] = digits.get(i).toString().charAt(0);
+            occurrencesLookUp.put(digits.get(i), occurrencesLookUp.get(digits.get(i)) - 1);
+            permutation(digits, occurrencesLookUp, combinationResult, recursionLevel+1,allSiblings);
+            occurrencesLookUp.put(digits.get(i), occurrencesLookUp.get(digits.get(i)) + 1);
+        }
+
+    }
+
+    private static Map<Integer, Integer> getIntAndCount(String input) {
+        Map<Integer, Integer> intAndCount = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            char charToTest = input.charAt(i);
+            if (Character.isDigit(charToTest)) {
+                int key = Character.getNumericValue(charToTest);
+                if (intAndCount.containsKey(key)) {
+                    intAndCount.put(key, intAndCount.get(key) + 1);
+                } else {
+                    intAndCount.put(key, 1);
+                }
+            }
+        }
+        return intAndCount;
+    }
+
+
     public static void main(String args[]) {
-        solution("326");
+        solution("632");
     }
 
 }
