c71fdc0699142ca7e341d60bfba0079586e54b30
baizel <baizelmathew@yahoo.co.in>
2020-01-30 18:20:33 +0000

    Init Commit

---
+++ src/main/java/com/and/test/Solution.java
@@ -0,0 +1,21 @@
+package com.and.test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution {
+
+    /**
+     * The following is the method where the solution shall be written
+     */
+
+    public static String solution(String input) throws NumberFormatException {
+        return null;
+    }
+
+    public static void main(String args[]) {
+        solution("326");
+    }
+
+}
