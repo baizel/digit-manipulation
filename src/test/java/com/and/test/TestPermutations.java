3449416c229325fbf73a00733b6bf8d4f477de3d
baizel <baizelmathew@yahoo.co.in>
2020-01-31 00:01:42 +0000

    Add more test

--- src/test/java/com/and/test/TestPermutations.java
+++ src/test/java/com/and/test/TestPermutations.java
@@ -2,29 +2,43 @@ package com.and.test;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPermutations {
+    @Test
+    void testPermutationCorrectness() {
+        String input = "236";
+        String expected = "632,623,362,326,263,236";
+        assertEquals(Solution.solution(input), expected);
+    }
+
+    @Test
+    void testPermutationCorrectnessWithDuplicates() {
+        String input = "112";
+        String expected = "211,121,112";
+        assertEquals(Solution.solution(input), expected);
+    }
+
     @Test
     void testPermutationsWithNoDuplicates() {
         String input = "325"; // 3!
         int numberOfCombinations = 6;
-        assertEquals(Solution.solution(input).split(",").length,numberOfCombinations);
+        assertEquals(Solution.solution(input).split(",").length, numberOfCombinations);
     }
 
     @Test
     void testPermutationsWithDuplicates() {
         String input = "3325";
         int numberOfCombinations = 12; // 4!/2!
-        assertEquals(Solution.solution(input).split(",").length,numberOfCombinations);
+        assertEquals(Solution.solution(input).split(",").length, numberOfCombinations);
     }
+
     @Test
     void testPermutationOrder() {
         String input = "236";
         String[] combs = Solution.solution(input).split(",");
-        for (int i=0; i< combs.length-1;i++){
-            assertTrue(Integer.parseInt(combs[i]) > Integer.parseInt(combs[i+1]));
+        for (int i = 0; i < combs.length - 1; i++) {
+            assertTrue(Integer.parseInt(combs[i]) > Integer.parseInt(combs[i + 1]));
         }
     }
 
@@ -32,8 +46,18 @@ public class TestPermutations {
     void testPermutationOrderWithDuplicates() {
         String input = "3325";
         String[] combs = Solution.solution(input).split(",");
-        for (int i=0; i< combs.length-1;i++){
-            assertTrue(Integer.parseInt(combs[i]) > Integer.parseInt(combs[i+1]));
+        for (int i = 0; i < combs.length - 1; i++) {
+            assertTrue(Integer.parseInt(combs[i]) > Integer.parseInt(combs[i + 1]));
         }
     }
+
+    @Test
+    void testWithNoIntegers() {
+        String input = "no integers";
+        assertThrows(NumberFormatException.class, ()->{
+            Solution.solution(input);
+        });
+    }
+
+
 }
